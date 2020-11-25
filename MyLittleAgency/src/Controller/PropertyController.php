5d4185a4b7212b86328267ee7c178e20be7a107c
Jérémy Lao <lao.jeremy@live.fr>
2020-11-25 16:33:16 +0100

    Database

---
+++ MyLittleAgency/src/Controller/PropertyController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+
+class PropertyController extends AbstractController
+{
+    public function index():Response
+    {
+        return $this->render('property/index.html.twig',
+            ['current_menu' => 'properties']
+        );
+    }
+}
