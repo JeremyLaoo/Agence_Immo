ece9dac1424c9dfe29ecccf2038bd1d18d2d6970
Jérémy Lao <lao.jeremy@live.fr>
2020-11-27 00:24:37 +0100

    add Oke

--- MyLittleAgency/src/Controller/PropertyController.php
+++ MyLittleAgency/src/Controller/PropertyController.php
@@ -1,15 +1,31 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class PropertyController extends AbstractController
 {
     public function index():Response
     {
+        $property = new Property();
+        $property->setTitle('Appartement T3 à Vitry Sur Seine')
+            ->setPrice(250000)
+            ->setRooms(3)
+            ->setBedrooms(2)
+            ->setDescription("My Little Agency vous propose à deux pas des transports, commerces et écoles, dans un secteur calme et arboré cet appartement de trois pièces comprenant : une entrée avec placard , un salon, une cuisine équipée, deux chambres, une salle de bains, WC séparés .Vous pourrez également profiter d'un balcon sans vis à vis")
+            ->setSurface(68)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Vitry Sur Seine')
+            ->setAddress('22 avenue Lucien Francais')
+            ->setPostalCode('94400');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
         return $this->render('property/index.html.twig',
             ['current_menu' => 'properties']
         );
