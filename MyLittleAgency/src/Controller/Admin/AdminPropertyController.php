c7f1905348d3e1e95cee6911d0fe7559b3b77b27
Jérémy Lao <lao.jeremy@live.fr>
2020-12-03 23:40:42 +0100

    CRUD

--- MyLittleAgency/src/Controller/Admin/AdminPropertyController.php
+++ MyLittleAgency/src/Controller/Admin/AdminPropertyController.php
@@ -7,6 +7,7 @@ use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -51,6 +52,7 @@ class AdminPropertyController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
             $this->em->persist($property);
             $this->em->flush();
+            $this->addFlash('success', 'Création réussie');
             return $this->redirectToRoute('admin.property.index');
         }
         return $this->render('admin/property/new.html.twig', [
@@ -73,6 +75,7 @@ class AdminPropertyController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $this->em->flush();
+            $this->addFlash('success', 'Modification du bien faites avec succès');
             return $this->redirectToRoute('admin.property.index');
         }
 
@@ -82,15 +85,18 @@ class AdminPropertyController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
-//     * @param Property $property
-//     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-//     */
-//    public function delete(Property $property) {
-//        $this->em->remove($property);
-//        $this->em->flush();
-//        return $this->redirectToRoute('admin.property.index');
-//    }
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
+     * @param Property $property
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function delete(Property $property, Request $request) {
+        if($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'La suppression a bien été effectué');
+        }
+        return $this->redirectToRoute('admin.property.index');
+    }
 }
 
